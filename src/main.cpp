d6fa9cfc369104e26e415c1b7595df5c824a6224
Casey Chance <caseyjchance@gmail.com>
2015-04-16 21:25:47 -0400

    Cleanly constructed

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
+#include "emitter.h"
 
 int main(int argc, char **argv){
     if(argc > 1){
         std::printf("You said: %s", argv[1]);
+        std::shared_ptr<MagicStuff::Emitter> em = std::shared_ptr<MagicStuff::Emitter>();
+        char p[] = "pop";
+        em->emit(p);
     } else {
         std::printf("You said nothing");
     }
